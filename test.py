2250f6317f31774249b09579aaf56d707ec82df3
Yamen Marquez <yamenmarquez@gmail.com>
2018-05-24 22:48:48 -0500

    WIP Posicionando los valores en la plantilla pdf

--- test.py
+++ test.py
@@ -7,18 +7,20 @@ from pdfrw import PdfReader
 from pdfrw.buildxobj import pagexobj
 from pdfrw.toreportlab import makerl
  
-producto = 'producto'
-lote = 'lote'
-cantidad_producida = 0
-viscosidad = 0
-pH = 0
-color = 'color'
-olor = 'olor'
-fecha = 'fecha'
+# producto = 'producto'
+# lote = 'lote'
+# cantidad_producida = 0
+# viscosidad = 0
+# pH = 0
+# color = 'color'
+# olor = 'olor'
+# fecha = 'fecha'
 
 wb = load_workbook(filename = 'coa.xlsx')
 
 sheet_coadb = wb['coa_db']
+
+inch = 72
 
 # for row in sheet_coadb.iter_rows(min_row=1, max_col=8, max_row=3):
 #     for cell in row:
@@ -31,11 +33,10 @@ col_status = sheet_coadb['I']
 for cell in col_status:
     if cell.value == 'Nuevo':
         # print(cell.value)
-        print(cell.row)
+        # print(cell.row
         row_number = cell.row
         col_number = cell.col_idx
-        sheet_coadb.cell(row=row_number, column=col_number, value='Viejo')
-        # print(row_number)
+        # sheet_coadb.cell(row=row_number, column=col_number, value='Viejo')
         row = sheet_coadb[row_number]
         for cell in row:
             if cell.column == 'A':
@@ -54,23 +55,23 @@ for cell in col_status:
                 olor = cell.value
             elif cell.column == 'H':
                 fecha = cell.value.date().strftime("%Y/%m/%d")
-    if producto != 'producto':
-        print(producto)
-        print(lote)
-        print(cantidad_producida)
-        print(viscosidad)
-        print(pH)
-        print(color)
-        print(olor)
-        print(fecha)
-    pdf_name = lote + '.pdf'
-    c = canvas.Canvas(pdf_name)
-    template = PdfReader('template_sh_fases.pdf',decompress=False).pages
-    t = pagexobj(template[0])
-    c.doForm(makerl(c, t))
-    c.drawString(400,650, producto)
-    c.showPage()
-    c.save()
+    
+        # print(producto)
+        # print(lote)
+        # print(cantidad_producida)
+        # print(viscosidad)
+        # print(pH)
+        # print(color)
+        # print(olor)
+        # print(fecha)
+        pdf_name = producto + lote + '.pdf'
+        c = canvas.Canvas(pdf_name)
+        template = PdfReader('template_sh_fases.pdf',decompress=False).pages
+        t = pagexobj(template[0])
+        c.doForm(makerl(c, t))
+        c.drawString(1.8*inch, 8.91*inch, producto)
+        c.showPage()
+        c.save()
 
 
                 
